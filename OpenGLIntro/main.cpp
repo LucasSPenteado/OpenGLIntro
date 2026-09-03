2421e9d67d15bdf83712d809c4cdee947dc0af70
Lucas Silva Penteado <lucasspenteado@hotmail.com>
2026-09-03 16:48:45 +0100

    Animate triangle color, switch to single triangle draw
    - Updated shaders: vertex uses vec4(aPos, 1.0), fragment uses uniform ourColor
    - Animated green channel of triangle color via uniform in render loop
    - Switched from rectangle (two triangles) to single triangle vertex array
    - Changed draw call from glDrawElements to glDrawArrays
    - Commented out wireframe mode and previous color assignments

--- OpenGLIntro/main.cpp
+++ OpenGLIntro/main.cpp
@@ -10,16 +10,22 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core \n"
 "layout (location = 0) in vec3 aPos; \n"
+"out vec4 vertexColor;\n"
 "void main() \n"
 "{ \n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+"	gl_Position = vec4(aPos, 1.0); \n"
+"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0); \n"
 "} \0";
 
 const char *fragmentShaderSource = "#version 330 core \n"
 "out vec4 FragColor; \n"
+//"in vec4 vertexColor; \n"
+"uniform vec4 ourColor;"
 "void main() \n"
 "{ \n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+//"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+//"	FragColor = vertexColor; \n"
+"	FragColor = ourColor; \n"
 "} \0";
 
 
@@ -82,11 +88,18 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	//Coordinates for two tringles to form a rectangle
+	//float vertices[] = {
+	//	0.5f,  0.5f, 0.0f,  // top right
+	//	0.5f, -0.5f, 0.0f,  // bottom right
+	//	-0.5f, -0.5f, 0.0f,  // bottom left
+	//	-0.5f,  0.5f, 0.0f   // top left 
+	//};
+
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
 	};
 
 	unsigned int indices[] = {
@@ -110,7 +123,8 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//To only show the outline of the vertices
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -119,8 +133,16 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); To draw both tirangles to form a rectangle
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
